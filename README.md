d47b29d96e9285f61d4aac2da2db577e4693a2cf
rfong <rachefo@gmail.com>
2019-10-15 15:12:46 -0700

    datavis transform

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # city-pulse
 
-Me just playing around with city datavis and getting to know my city better after reading about factors contributing to city vitality in Jane Jacobs' *Death and Life of Great American Cities*.
+Me just playing around with city datavis after reading about factors contributing to city vitality in Jane Jacobs' *Death and Life of Great American Cities*.
+
+### Thoughts/todos
+
+[ ] The general physical distribution of businesses dominates the actual point values. Maybe try normalizing by density where appropriate?
+[ ] Residency data - SFGov / Trulia
---
+++ data/datavis_transform.py
@@ -0,0 +1,85 @@
+'''
+Script to transform downloaded data to a format usable for my datavis.
+This assumes you have already run the fetcher script and have some data
+downloaded.
+
++ heatmap points: `[lat, lng, intensity]`
+'''
+
+import json
+import os
+import pprint
+import time
+
+from yelp.settings import *
+
+
+def to_points(
+    value_selector, value_transform_fn=None, ignore_nulls=True,
+    restrict_to_city=None
+    ):
+    '''
+    Transforms business data into a list of points to be used on a map; that
+    is, coordinates with an associated value.
+
+    `value_selector` is a dictionary key into a Yelp business API object.
+    Point format:
+        `[lat, lng, value]`
+    '''
+    if value_transform_fn is None:
+        value_transform_fn = lambda x: x  # Identity function
+    print(set(b.get('location',{}).get('city') for id, b in list(get_business_data().items())))
+    return [
+        [
+            b['coordinates']['latitude'],
+            b['coordinates']['longitude'],
+            value_transform_fn(b.get(value_selector)),
+        ]
+        for id, b in list(get_business_data().items())
+        if ((not ignore_nulls or value_selector in b) and
+            (b['location']['city'].lower().replace(' ', '') == 
+             restrict_to_city.lower().replace(' ', '')
+             if restrict_to_city else True))
+    ]
+
+def write_heatmap_points(rel_path, points):
+    path = os.path.join(os.path.dirname(__file__), rel_path)
+    with open(path, 'w') as f:
+        f.write(json.dumps(points))
+
+
+business_data_cache = None
+business_data_cached_path = None
+
+def get_business_data(path=None):
+    '''
+    Fetch all business data from local storage.
+    '''
+    if path is None:
+        path = SEARCH_API_DATA_PATH
+
+    global business_data_cache, business_data_cached_path
+    if business_data_cache is None or path != business_data_cached_path:
+        with open(path, 'r') as f:
+            business_data_cache = json.loads(f.read())
+            business_data_cached_path = path
+
+    return business_data_cache
+
+
+if __name__ == '__main__':
+    city = 'San Francisco'
+    write_heatmap_points(
+        'yelp_price_points.json',
+        to_points('price', lambda price: len(price), restrict_to_city=city)
+    )
+    
+    write_heatmap_points(
+        'yelp_rating_points.json',
+        to_points('rating', restrict_to_city=city)
+    )
+    
+    write_heatmap_points(
+        'yelp_review_count_points.json',
+        to_points('review_count', restrict_to_city=city)
+    )
